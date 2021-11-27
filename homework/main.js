0646cff83f4b1f92638f96f5aa3c1bd401950aa0
Turxan <90092145+Asgarov035@users.noreply.github.com>
2021-11-27 08:13:44 +0400

    Add files via upload

---
+++ homework/main.js
@@ -0,0 +1,69 @@
+// 1ci tapsirig
+// let num=prompt("Xahis olunur ededi daxil edin")
+
+// if (num>0 && num<=1) {
+//     alert("Secdiyiniz edede uygun yaz feslidir.")
+    
+// }
+// if (num>1 && num<=2) {
+//     alert("Secdiyiniz edede uygun yay feslidir.")
+    
+// }if (num>2 && num<=3) {
+//     alert("Secdiyiniz edede uygun payiz feslidir.")
+    
+// }if (num>3 && num<=4) {
+//     alert("Secdiyiniz edede uygun qis feslidir.")
+    
+// }
+
+
+
+
+// 2ci tapsirig
+
+
+// let num=prompt("Xahis olunur ededi daxil edin")
+// for (let result = 0; result <= num; result++) {
+//     if (num%result==0) {
+//         console.log(result)
+//     }
+    
+// }
+
+
+
+// 3cu tapsirig
+
+
+// let max = 5;
+// let i;
+// let  array = new Array(max);
+
+
+// for (i = 1; i <= max; i++) {
+//     array[i] = prompt("5 dene eded daxil edin");
+// }
+
+
+// array.sort(function (a, b) {
+//     return b - a
+// });
+
+
+// let  determined_max = array[0];
+
+// alert("Daxil etdiyiniz en boyuk eded " + determined_max);
+
+
+
+
+
+
+// let inputNumber = prompt('Faktorilani hesablamag isdediyiniz ededi daxil edin ');
+// let total = 1;
+
+// for (i = 0; i < inputNumber; i++){
+//     total = total * (inputNumber - i);
+// }
+
+// alert(inputNumber + '! = ' + total);
